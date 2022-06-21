2f547abba7d8f6d67ef69d2078bde2789b310c1f
Hoang An <antnh@icd-vn.com>
2022-06-21 09:15:35 +0700

    make it better

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ app.use('/api', require('./routes/auth.route'))
 app.use('/api', require('./routes/category.route'))
 app.use('/api', require('./routes/user.route'))
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
