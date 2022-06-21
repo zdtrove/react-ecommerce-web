560727edc9d1306ca34e8db5b2814abee2b4b711
Hoang An <antnh@icd-vn.com>
2022-06-21 08:41:25 +0700

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import { getLoggedUser } from './redux/actions/auth.action'
 
 function App() {
   const dispatch = useDispatch()
+  console.log('app');
 
   useEffect(() => {
     dispatch(getLoggedUser())
