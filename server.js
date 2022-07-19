6900dc2bda8f1fb3f7d158a38d5887d5d897017a
Hoang An <antnh@icd-vn.com>
2022-07-19 16:43:15 +0700

    Commit fix error path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 app.use(express.json())
